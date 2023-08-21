d1c7b550b670dabf7d964af0a439e3f6aa00a38e
Omonjon <omonjons077@gmail.com>
2023-08-21 10:52:42 +0500

    from Intro to FAQs modified

--- assets/script/main.js
+++ assets/script/main.js
@@ -25,6 +25,55 @@ if ($("#myHeader").length) {
     }
 }
 
+
+/*=============== TESTIMONIAL | SWIPER ===============*/
+if ($(".testimonial").length) {
+var swiper = new Swiper(".testimonial", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    autoplay: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 575px
+        575: {
+            slidesPerView: 2,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 3,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+        // when window width is >= 992px
+        992: {
+            slidesPerView: 4,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+         // when window width is >= 1200px
+         1200: {
+            slidesPerView: 5,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+         // when window width is >= 1440px
+         1440: {
+            slidesPerView: 6,
+            // spaceBetween: 0,
+            // slideToClickedSlide: true,
+        },
+    }
+  });
+};
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
